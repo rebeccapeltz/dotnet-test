ddcf15964a49d1612f2fee5e6ced5351c4c64304
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-05-20 15:43:51 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.IO;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+
+namespace myApp
+{
+    class Program
+    {
+       static void Main(string[] args)
+				{
+					static Dictionary<string, string> DeserializeJson (string json)
+				{
+						return JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+				}
+					static Dictionary<string, string> GetCredentials()
+				{
+						using (StreamReader r = new StreamReader("appsetting.json"))
+						{
+								string json = r.ReadToEnd();
+							return DeserializeJson(json);
+					}
+				}
+		var credentials = GetCredentials();
+        var myurl = credentials["CLOUDINARY_URL"];
+        Console.WriteLine(myurl);
+		Cloudinary cloudinary = new Cloudinary(myurl);
+        cloudinary.Api.Timeout = int.MaxValue;
+		 var explicitParams = new ExplicitParams("sample"){
+			  Type = "upload",
+		 	  Invalidate = true};
+			var explicitResult = cloudinary.Explicit(explicitParams);
+            Console.WriteLine(explicitResult.SecureUri);
+}
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+1384  dotnet add package Newtonsoft.Json
+ 1385  dotnet run
+ 1386  dotnet add package CloudinaryDotNet
+ 1387  dotnet add package CloudinaryDotNet.Actions
+ dotnet add package Microsoft.Extensions.Configuration --version 3.1.4
+
+ https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-using-the-dotnet-cli
+
+ https://dotnet.microsoft.com/platform/tools
+
+ https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/intro
+
+ 
